7b7c675dabd0a1b79802cf34ab6e0c10f76648fb
Sanorun <sanorun974@gmail.com>
2025-03-15 18:14:29 +0400

    Add asteroid and asteroidfield classes

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -9,22 +12,42 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+   
     dt = 0
 
+    
+
     while True:
+        # Calculate dt first
+        dt = clock.tick(60) / 1000
+    
+        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        player.update(dt)
-        
+    
+        # Update game state with the calculated dt
+        upadatable.update(dt)
+    
+        # Render the game
         screen.fill("black")
-        player.draw(screen)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
-
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
